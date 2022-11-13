f3e878009a6076f8d8348c977fd785e9b36e2808
ahmad.albaqir <ahmad.albaqir@gojek.com>
2022-11-13 16:01:02 +0700

    Set result query to user model

--- src/usecase/UserUsecase.java
+++ src/usecase/UserUsecase.java
@@ -8,6 +8,7 @@ import javax.naming.spi.DirStateFactory.Result;
 
 import dto.LoginDTO;
 import dto.RegisterDTO;
+import model.UserModel;
 public class UserUsecase {
 	private Connection conn;
 	public UserUsecase(Connection conn) {
@@ -26,17 +27,23 @@ public class UserUsecase {
 		}
 	}
 
-	public void login(LoginDTO payload) throws Exception {
+	public UserModel login(LoginDTO payload) throws Exception {
+		UserModel user = new UserModel();
 		try {
-			PreparedStatement stmt = this.conn.prepareStatement("SELECT * FROM users LEFT JOIN profile ON profile.id_users = users.id WHERE username = ? AND password = ?");
+			PreparedStatement stmt = this.conn.prepareStatement("SELECT users.id, users.username, profile.name, profile.address, profile.phone_number FROM users LEFT JOIN profile ON profile.id_users = users.id WHERE username = ? AND password = ?");
 			stmt.setString(1, payload.getUsername());
 			stmt.setString(2, payload.getPassword());
 			ResultSet results =  stmt.executeQuery();
 			while (results.next()) {
-				
+				user.setIdUser(results.getInt("id"));
+				user.setAddress(results.getString("address"));
+				user.setName(results.getString("name"));
+				user.setUsername(results.getString("username"));
+				user.setPhoneNumber(results.getString("phone_number"));
 			}
 		} catch (Exception e) {
 			throw e;
 		}
+		return user;
 	}
 }
